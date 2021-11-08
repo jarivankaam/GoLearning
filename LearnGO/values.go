818d93a9ec865cd3c69d037740a03453318da187
jarivankaam <71122219+jarivankaam@users.noreply.github.com>
2021-11-08 09:23:39 +0100

    Added values.go

---
+++ LearnGO/values.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("go" + "lang")
+	//concatenate go and lang
+
+	fmt.Println("1+1 =", 1+1)
+	//make a string and then add de calculated integer at the end
+
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+	//make a string and then add the calculated float at the end
+
+
+	fmt.Println(true && false)
+	//bools
+
+	fmt.Println(true || false)
+	//bools
+
+	fmt.Println(!true)
+	//bools
+}
