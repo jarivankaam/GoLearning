2568af5ffebc636d076bf7165adca5c7249b0bcb
jarivankaam <71122219+jarivankaam@users.noreply.github.com>
2021-11-08 10:06:17 +0100

    added notes in for loops

--- LearnGO/for.go
+++ LearnGO/for.go
@@ -8,18 +8,24 @@ func main() {
 		fmt.Println(i)
 		i = i +1
 	}
+	//single condition for loop
 
 	for j := 7; j <+ 9; j++{
 		fmt.Println(j)
 	}
+	// initial/condition/after for loop
+
 	for {
 		fmt.Println("loop")
 		break
 	}
+	// not conditional for loop with break
+
 	for n := 0; n <= 5; n++{
 		if n%2 == 0{
 		continue}
 		fmt.Println(n)
 	}
+	// continues after next iteration
 }
 
