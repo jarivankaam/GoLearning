b689ee935b174ff0e44d30ee89057a6fc312ba1b
jarivankaam <71122219+jarivankaam@users.noreply.github.com>
2021-11-08 09:32:26 +0100

    added variables.go

---
+++ LearnGO/variables.go
@@ -0,0 +1,35 @@
+package main
+
+import"fmt"
+
+func main() {
+	var a = "initial"
+	//make a var and fill it with "initial"
+
+	fmt.Println(a)
+	//print out var a
+
+	var b, c int = 1,2
+	//declare var b and c en fill them both with an int
+
+	fmt.Println(b,c)
+	//print out b and c
+
+	var d = true
+	//declare var b and fill it with the true statement
+
+	fmt.Println(d)
+	//print out d
+
+	var e int
+	//declare a int var called e
+
+	fmt.Println(e)
+
+	f := "apple"
+	//shorthand for declaring a var without needing to put var
+
+	fmt.Println(f)
+	//print out f
+
+}
